74c891fb0241b840ebd6c48025e1549b7cf76985
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-14 19:29:18 +0300

    auto-layout using anchoring

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -49,32 +49,53 @@ class ViewController: UIViewController {
         view.addSubview(label5)
         
         
-        // we need to create a dictionary of the views we want to lay out
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4 , "label5": label5]
-        
-        //view.addConstraints():  adds an array of constraints to our view controller's view.
-       // NSLayoutConstraint.constraints(withVisualFormat:) Auto Layout method  converts VFL into an array of constraints
-        
-        for label in viewsDictionary.keys {
-            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
-            //H means horizontal, | means edge of view
-        }
-        
-        //v for vertial, - space by default 10 points ,
-        //our vertical VFL doesn't have a pipe at the end
-        //we're not forcing the last label to stretch all the way to the edge
-        //This will leave whitespace after the last label
-        
-        //when specifying the size of a space, you need to use the - before and after the size -(>=10)-
-       
-        //constraints are evaluated from highest priority down to lowest
-        //1000 required anything less optional
-        //tell Auto Layout that we want all the labels to have the same height. This is important, because if all of them have optional heights using labelHeight, Auto Layout might solve the layout by shrinking one label and making another 88
-        //making them have same height (label1)
-        let metrics = ["labelHeight": 88]
-        
-        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+//        // we need to create a dictionary of the views we want to lay out
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4 , "label5": label5]
+//
+//        //view.addConstraints():  adds an array of constraints to our view controller's view.
+//       // NSLayoutConstraint.constraints(withVisualFormat:) Auto Layout method  converts VFL into an array of constraints
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//            //H means horizontal, | means edge of view
+//        }
+//
+//        //v for vertial, - space by default 10 points ,
+//        //our vertical VFL doesn't have a pipe at the end
+//        //we're not forcing the last label to stretch all the way to the edge
+//        //This will leave whitespace after the last label
+//
+//        //when specifying the size of a space, you need to use the - before and after the size -(>=10)-
+//
+//        //constraints are evaluated from highest priority down to lowest
+//        //1000 required anything less optional
+//        //tell Auto Layout that we want all the labels to have the same height. This is important, because if all of them have optional heights using labelHeight, Auto Layout might solve the layout by shrinking one label and making another 88
+//        //making them have same height (label1)
+//        let metrics = ["labelHeight": 88]
+//
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
 
+        
+        
+        var previous: UILabel?
+
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+
+            if let previous = previous {
+                // we have a previous label – create a height constraint
+                //combines a different anchor with a constant value (10) to get spacing between the views
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            }
+            else {
+                // this is the first label
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+
+            // set the previous label to be the current one, for the next loop iteration
+            previous = label
+        }
         
     }
     
